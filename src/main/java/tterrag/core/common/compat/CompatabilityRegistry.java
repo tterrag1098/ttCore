2d0bc0a88402603c80b9a12c611f4e98dc7979c3
tterrag1098 <tterrag1098@gmail.com>
2014-09-26 20:49:47 -0400

    InvocationTargetException now crashes the game.

--- src/main/java/tterrag/core/common/compat/CompatabilityRegistry.java
+++ src/main/java/tterrag/core/common/compat/CompatabilityRegistry.java
@@ -100,6 +100,8 @@ public class CompatabilityRegistry
                 TTCore.logger.error("[Compat] Error {}", i);
                 suppressed[i].printStackTrace();
             }
+            
+            throw new RuntimeException("Error in compatability class " + clazz);
         }
         catch (Exception e)
         {
