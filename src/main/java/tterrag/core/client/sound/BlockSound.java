c632a02f588658c95683f81bc59bf892b256194f
tterrag1098 <tterrag1098@gmail.com>
2014-10-17 03:41:59 -0400

    Add BlockSound

---
+++ src/main/java/tterrag/core/client/sound/BlockSound.java
@@ -0,0 +1,64 @@
+package tterrag.core.client.sound;
+
+import net.minecraft.client.audio.ITickableSound;
+import net.minecraft.client.audio.PositionedSound;
+import net.minecraft.util.ResourceLocation;
+
+public class BlockSound extends PositionedSound implements ITickableSound
+{
+    private boolean donePlaying = false;
+    
+    public BlockSound(ResourceLocation p_i45103_1_)
+    {
+        super(p_i45103_1_);
+    }
+    
+    public BlockSound setVolume(float vol)
+    {
+        this.volume = vol;
+        return this;
+    }
+    
+    public BlockSound setPitch(float pitch)
+    {
+        this.field_147663_c = pitch;
+        return this;
+    }
+
+    @Override
+    public void update()
+    {
+        ;
+    }
+
+    @Override
+    public boolean isDonePlaying()
+    {
+        return donePlaying;
+    }
+    
+    public BlockSound setDonePlaying(boolean bool)
+    {
+        this.donePlaying = bool;
+        return this;
+    }
+    
+    public boolean getDonePlaying()
+    {
+        return donePlaying;
+    }
+
+    public BlockSound setLocation(float x, float y, float z)
+    {
+        this.xPosF = x;
+        this.yPosF = y;
+        this.zPosF = z;
+        return this;
+    }
+
+    public BlockSound setDoRepeat(boolean bool)
+    {
+        this.repeat = bool;
+        return this;
+    }
+}
