3a2b3bc7dae1f51abeec4f9fe619b6dd4e0cb412
tterrag1098 <tterrag1098@gmail.com>
2014-12-30 18:45:49 -0500

    Fix right clicking crops not dropping everything it should curse you magical crops

--- src/main/java/tterrag/core/common/handlers/RightClickCropHandler.java
+++ src/main/java/tterrag/core/common/handlers/RightClickCropHandler.java
@@ -1,21 +1,21 @@
 package tterrag.core.common.handlers;
 
-import java.util.List;
-
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemSeeds;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import tterrag.core.common.Handlers.Handler;
 import tterrag.core.common.Handlers.Handler.HandlerType;
 import tterrag.core.common.config.ConfigHandler;
-import tterrag.core.common.util.TTItemUtils;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 @Handler(HandlerType.FORGE)
 public class RightClickCropHandler
 {
+    private boolean doRemoveSeed = false;
+    
     @SubscribeEvent
     public void handleCropRightClick(PlayerInteractEvent event)
     {
@@ -30,23 +30,29 @@ public class RightClickCropHandler
             }
             else
             {
-                ItemStack held = event.entityPlayer.getCurrentEquippedItem();
-                List<ItemStack> drops = block.getDrops(event.world, x, y, z, meta, 0);
-                boolean seedRemoved = false;
-                for (ItemStack stack : drops)
-                {
-                    // TODO add some kind of json config for different values, mainly what the seed is
-                    if (!(stack.getItem() instanceof ItemSeeds && !seedRemoved))
-                    {
-                        TTItemUtils.spawnItemInWorldWithRandomMotion(event.world, stack, x, y, z);
-                    }
-                    else
-                    {
-                        seedRemoved = true;
-                    }
-                }
+                doRemoveSeed = true;
+                block.dropBlockAsItem(event.world, x, y, z, meta, 0);
+                doRemoveSeed = false;
                 event.world.setBlockMetadataWithNotify(x, y, z, 0, 3);
                 event.setCanceled(true);
+            }
+        }
+    }
+    
+    @SubscribeEvent
+    public void onHarvestDrop(HarvestDropsEvent event)
+    {
+        if (doRemoveSeed)
+        {
+            for (int i = 0; i < event.drops.size(); i++)
+            {
+                ItemStack stack = event.drops.get(i);
+                // TODO add some kind of json config for different values, mainly what the seed is
+                if (stack.getItem() instanceof ItemSeeds)
+                {
+                    event.drops.remove(i);
+                    break;
+                }
             }
         }
     }
